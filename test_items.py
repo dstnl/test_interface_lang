3911464ae0e41cf2a022da9f08f295a7d31873ec
dstnl <dstnl@users.noreply.github.com>
2022-04-14 22:35:05 +0300

    Added test for busket button

---
+++ test_items.py
@@ -0,0 +1,15 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    time.sleep(15)
+    assert button.is_displayed() == True, "No 'Add to basket' button on this page"
+
+
+
