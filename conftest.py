9514fef8bc2fa5dff98df087e313bbd5a4e45780
dstnl <dstnl@users.noreply.github.com>
2022-04-14 23:41:50 +0300

    Added language support

--- conftest.py
+++ conftest.py
@@ -3,28 +3,16 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default=None,
                      help="Choose language:")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("--browser_name")
     user_language = request.config.getoption("--language")
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        options = webdriver.ChromeOptions()
-        options.add_argument("--lang=" + user_language)
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        fp = Options()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(options=fp)
-        print("\nstart firefox browser for test..")
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    options = webdriver.ChromeOptions()
+    options.add_argument("--lang=" + user_language)
+    browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
     browser.quit()
