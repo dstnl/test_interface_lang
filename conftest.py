3e19525d94c7c738c60c9a539535b217e3cfdf96
dstnl <dstnl@users.noreply.github.com>
2022-04-14 22:38:14 +0300

    Added interface language change

--- conftest.py
+++ conftest.py
@@ -1,29 +1,27 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
-    parser.addoption('--user_language', action='store', default=None,
+    parser.addoption('--language', action='store', default=None,
                      help="Choose language:")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("user_language")
-    browser = None
-    user_language = None
+    browser_name = request.config.getoption("--browser_name")
+    user_language = request.config.getoption("--language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
         options = webdriver.ChromeOptions()
-        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        options.add_argument("--lang=" + user_language)
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
-        fp = webdriver.FirefoxProfile()
+        fp = Options()
         fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
+        browser = webdriver.Firefox(options=fp)
         print("\nstart firefox browser for test..")
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
