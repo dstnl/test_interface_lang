aa88813e16f10bfe6e68ac91f783f492ca524af5
dstnl <dstnl@users.noreply.github.com>
2022-04-14 23:07:41 +0300

    Added temp changes

--- test_items.py
+++ test_items.py
@@ -7,8 +7,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_add_to_basket_button(browser):
     browser.get(link)
+    time.sleep(30)
     button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-    time.sleep(15)
     assert button.is_displayed() == True, "No 'Add to basket' button on this page"
 
 
